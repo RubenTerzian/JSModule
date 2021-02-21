57600f8b6e57a2f71f5f6ea0be1ec9155b5d425d
Ruben <rbr2063@gmail.com>
2021-02-21 21:19:59 +0200

    in progress

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const calendarEvents = document.querySelector('.plan');
 const modalWindow = document.querySelector('#modal')
 
 
-const eventArray = [
+const eventDataArray = [
+    // {start: 0, duration: 200, title: "Go to gum after hard day"},
+    // {start: 60, duration: 60, title: "Go to fe"},
+    // {start: 130, duration: 30, title: "Go to 222"},
+    // {start: 170, duration: 210, title: "Go to m"},
+    // {start: 240, duration: 30, title: "asdgadsg"},
+    // {start: 300, duration: 30, title: "asdfadsf"},
+    // {start: 370, duration: 100, title: "Gsdfm"},
+];
+
+const arrayForRender = [
     // {start: 0, duration: 200, title: "Go to gum after hard day"},
     // {start: 60, duration: 60, title: "Go to fe"},
     // {start: 130, duration: 30, title: "Go to 222"},
@@ -15,6 +25,7 @@ const eventArray = [
 class Calendar{
 
     addEvent() {
+        let id = 1;
         // Show and hides inputs
         const eventInfo = document.querySelector(".event-info")
         const addEventButton = document.querySelector(".add-event");
@@ -35,8 +46,9 @@ class Calendar{
         const inpSetColor = document.querySelector('#input-set-color');
 
 
-        btnCreateEvent.addEventListener('click', e => {
-            const eventData = {start: '', duration: '', title: ''}
+        btnCreateEvent.addEventListener('click', () => {
+            const eventData = {}
+            const eventDataForRender = {}
 
             const validStartTime = (elem)=> {
                 let timeArr = elem.split(':');
@@ -44,25 +56,33 @@ class Calendar{
             }
             eventData.start = validStartTime(inpAddStartTime.value)-8*60
             eventData.duration = validStartTime(inpAddEndTime.value)-8*60 - eventData.start
-            eventData.title = inpAddTitle.value
+            eventData.title = inpAddTitle.value;
+            Object.assign(eventDataForRender, eventData);
+            eventDataForRender.color = inpSetColor.value;
+            eventDataForRender.startTime = inpAddStartTime.value;
+            eventDataForRender.endTime = inpAddEndTime.value;
             
-            if (inpAddStartTime.value < inpAddStartTime.min || inpAddStartTime.value > inpAddStartTime.max || inpAddEndTime.value < inpAddEndTime.min || inpAddEndTime.value > inpAddEndTime.max) {
+            if (inpAddStartTime.value < inpAddStartTime.min 
+                || inpAddStartTime.value > inpAddStartTime.max 
+                || inpAddEndTime.value < inpAddEndTime.min 
+                || inpAddEndTime.value > inpAddEndTime.max) {
                 alert("Your event should start no earlier than 8 am and end no later than 5 pm")
             } else {
                 if (inpAddStartTime.value >= inpAddEndTime.value) {
                     alert("The end time must be greater than the start time of the event")
                 } else {
-                    const currentEventsArray = eventArray.filter(el => 
+                    const currentEventsArray = arrayForRender.filter(el => 
                         el.start <= eventData.start+eventData.duration && el.start+el.duration >= eventData.start
                     )
-                    // ТУТ НАДО ДОПИЛИТЬ ЛОГИКУ
                     if (currentEventsArray.length > 1) {
-                        alert('to many')
+                        alert('Only 2 events at the same time')
                     } else {
-                        eventArray.push(eventData)
-                        eventArray.sort((a,b)=>a.start-b.start)
-                        this.renderEvents(inpSetColor.value, eventData);
-                        this.modal(inpSetColor.value, eventData);
+                        eventDataForRender.id = id++;
+                        eventDataArray.push(eventData)
+                        arrayForRender.push(eventDataForRender)
+                        arrayForRender.sort((a,b)=>a.start-b.start)
+                        this.renderEvents();
+                        this.modal();
                     }
                 }
             }
@@ -71,42 +91,53 @@ class Calendar{
         
     }
 
-    modal(eventColor, eventData) {
-        const eventsArray = document.querySelectorAll(".single-event, .double-event")
-        console.log(eventData)
-        let validStartTime =''
-        let validEndTime =''
-        eventData? validStartTime = ((Math.floor((eventData.start + 8 * 60) / 60))>9 ? Math.floor((eventData.start + 8 * 60) / 60): "0"+(Math.floor((eventData.start + 8 * 60) / 60))) + ":" + (((eventData.start + 8 * 60) % 60 == 0) ? "00":(eventData.start + 8 * 60) % 60):'';
-        eventData? validEndTime = ((Math.floor((eventData.start +eventData.duration + 8 * 60) / 60))>9 ? Math.floor((eventData.start + eventData.duration + 8 * 60) / 60): "0"+(Math.floor((eventData.start + eventData.duration + 8 * 60) / 60))) + ":" + (((eventData.start+eventData.duration + 8 * 60) % 60 == 0) ? "00":(eventData.start+eventData.duration + 8 * 60) % 60):'';
+    modal() {
+        const eventsArray = document.querySelectorAll(".single-event, .double-event");
         eventsArray.forEach(element => {
+            
+            console.log(arrayForRender)
             element.addEventListener('click', e => {
-                modalWindow.className = 'modal'
-                const modalContent = document.createElement('div')
-                modalContent.className = "modal-content"
-                modalContent.innerHTML = `
-                    
-                    <div>
-                        <h3>Name of event</h3>
-                        <input type="text" name="title" placeholder="Введите имя события..." id="modal-input-add-title" value="${eventData?eventData.title:''}">
-                    </div>
-                    <div>
-                        <h3>Start</h3>
-                        <input type="time" min="08:00" max="17:00" id="modal-input-add-start-time" value="${validStartTime}">
-                    </div>
-                    <div>
-                        <h3>End</h3>
-                        <input type="time" min="08:00" max="17:00" id="modal-input-add-end-time" value="${validEndTime}">
-                    </div>
-                    <div class="modal-color">
-                        <h3>Choose a color</h3>
-                        <input type="color" id="modal-input-set-color" value="#6E9ECF">
-                    </div>
-                    <button id="btn-change-event">Add event</button>
-                    <div class="delete-wraper">
-                        <img src="img/delete.png" alt="image" class="delete-btn">
-                    </div>
-                `
-                modalWindow.append(modalContent)
+            const eventForRender = arrayForRender.find(data=>{
+                if(e.target.id){
+                    return data.id == e.target.id
+                }else{
+                    return data.id == e.target.parentNode.id
+                }
+            });
+
+            // const eventData = eventDataArray.find(data=>{
+            //     data.title == eventForRender.title
+            // });
+            
+
+            const modalContent = document.createElement('div');
+            modalContent.innerHTML = `
+                
+                <div>
+                    <h3>Name of event</h3>
+                    <input type="text" name="title" placeholder="Введите имя события..." id="modal-input-add-title" value="${eventForRender.title}">
+                </div>
+                <div>
+                    <h3>Start</h3>
+                    <input type="time" min="08:00" max="17:00" id="modal-input-add-start-time" value="${eventForRender.startTime}">
+                </div>
+                <div>
+                    <h3>End</h3>
+                    <input type="time" min="08:00" max="17:00" id="modal-input-add-end-time" value="${eventForRender.endTime}">
+                </div>
+                <div class="modal-color">
+                    <h3>Choose a color</h3>
+                    <input type="color" id="modal-input-set-color" value="${eventForRender.color}">
+                </div>
+                <button id="btn-change-event">Save</button>
+                <div class="delete-wraper">
+                    <img src="img/delete.png" alt="image" class="delete-btn">
+                </div>
+            `
+            modalWindow.append(modalContent)
+
+                modalWindow.className = 'modal';
+                modalContent.className = "modal-content";
                 const deleteBtn = document.querySelector(".delete-btn");
                 modalWindow.addEventListener('click', event => {
                     if(event.target.className === 'modal'){
@@ -114,18 +145,17 @@ class Calendar{
                         event.target.className += " disabled";
                     };
                 })
-                this.removeEvent(deleteBtn, element, eventColor, eventData)
-                this.changeEvent(eventData, eventData)
+                this.removeEvent(deleteBtn, eventForRender)
+                this.changeEvent(eventForRender)
             })
         })
         
     }
 
-    renderEvents(eventColor, elem) {
+    renderEvents() {
         let previousEvent = ""
         calendarEvents.innerHTML = '';
-        const currnetEvent = elem;
-        eventArray.forEach((eventData, index, array) => {
+        arrayForRender.forEach((eventData, index, array) => {
             const event = document.createElement('div');
                 
             // Если это первый элемент
@@ -133,10 +163,10 @@ class Calendar{
                 // Если следующие событие начинается, пока предыдущее длиться
                 if (array.length>1 && eventData.start + eventData.duration > array[index+1].start) {
                     event.setAttribute('class', 'double-event');
-                    event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; `);
+                    event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; background-color:${eventData.color+"70"}; border-left: 3px solid ${eventData.color}`);
                 } else {
                     event.setAttribute('class', 'single-event');
-                    event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px;`);
+                    event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; background-color:${eventData.color+"70"}; border-left: 3px solid ${eventData.color}`);
                 }
                 previousEvent = event;
             } else {
@@ -146,13 +176,13 @@ class Calendar{
                 if (eventData.start < previousEventEndTime ||  index<array.length-1 && eventData.start + eventData.duration > array[index+1].start) {
                     event.setAttribute('class', 'double-event');
                     if (previousEvent.offsetLeft == 0) {
-                        event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; left: 50%;`);
+                        event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; left: 50%; background-color:${eventData.color+"70"}; border-left: 3px solid ${eventData.color}`);
                     } else {
-                        event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; `);
+                        event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; background-color:${eventData.color+"70"}; border-left: 3px solid ${eventData.color}`);
                     }
                 }else{
                     event.setAttribute('class', 'single-event');
-                    event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; `);
+                    event.setAttribute('style', `top: ${eventData.start*2}px; height: ${eventData.duration*2}px; background-color:${eventData.color+"70"}; border-left: 3px solid ${eventData.color}`);
                 }
 
                 // Если предыдущее событие уже закончилось, когда началось текущее; или конец текущего событие позже чем конец предыдущего события
@@ -161,30 +191,27 @@ class Calendar{
                 }
 
             }
-            if(currnetEvent == eventData){
-                event.style.backgroundColor = eventColor+"70";
-                event.style.borderLeft = `3px solid ${eventColor}`;
-            }
             event.innerHTML = ` <p>${eventData.title}</p> `;
+            event.id = eventData.id
             calendarEvents.append(event);
             
         });
 
     }
 
-    removeEvent(btn, elem, eventColor, eventData) {
+    removeEvent(btn, event) {
         btn.addEventListener('click', e => {
-            const removedEventIndex = eventArray.indexOf(elem);
-            eventArray.splice(removedEventIndex, 1)
+            const removedEventIndex = arrayForRender.indexOf(event);
+            arrayForRender.splice(removedEventIndex, 1)
             modalWindow.innerText='';
             modalWindow.className += " disabled";
-            this.renderEvents(eventColor,  elem)
-            this.modal(eventColor, eventData)
+            this.renderEvents(event)
+            this.modal(event)
         })
             
     }
 
-    changeEvent(elem, eventData){
+    changeEvent(event){
         const btnChangeEvent = document.querySelector('#btn-change-event');
         const validStartTime = (elem)=> {
             let timeArr = elem.split(':');
@@ -195,25 +222,28 @@ class Calendar{
             const inpAddStartTime = document.querySelector('#modal-input-add-start-time');
             const inpAddEndTime = document.querySelector('#modal-input-add-end-time');
             const inpSetColor = document.querySelector('#modal-input-set-color');
-            elem.start = validStartTime(inpAddStartTime.value)-8*60
-            elem.duration = validStartTime(inpAddEndTime.value)-8*60 - eventData.start
-            elem.title = inpAddTitle.value
 
-            if (inpAddStartTime.value < inpAddStartTime.min || inpAddStartTime.value > inpAddStartTime.max || inpAddEndTime.value < inpAddEndTime.min || inpAddEndTime.value > inpAddEndTime.max) {
+            if (inpAddStartTime.value < inpAddStartTime.min 
+                || inpAddStartTime.value > inpAddStartTime.max 
+                || inpAddEndTime.value < inpAddEndTime.min 
+                || inpAddEndTime.value > inpAddEndTime.max) {
                 alert("Your event should start no earlier than 8 am and end no later than 5 pm")
             } else {
                 if (inpAddStartTime.value >= inpAddEndTime.value) {
                     alert("The end time must be greater than the start time of the event")
                 } else {
-                        eventArray.sort((a,b)=>a.start-b.start)
-                        this.renderEvents(inpSetColor.value, elem)
-                        this.modal(inpSetColor.value, eventData);
+                        event.start = validStartTime(inpAddStartTime.value)-8*60
+                        event.duration = validStartTime(inpAddEndTime.value)-8*60 - event.start
+                        event.title = inpAddTitle.value;
+                        event.color = inpSetColor.value;
+                        arrayForRender.sort((a,b)=>a.start-b.start)
+                        this.renderEvents(event)
+                        this.modal(event);
+                        
+                        modalWindow.innerText='';
+                        modalWindow.className += " disabled";
                 }
             }
-            
-            // this.modal(eventColor, eventData)
-            modalWindow.innerText='';
-            modalWindow.className += " disabled";
         })
     }
 
